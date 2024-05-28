d41e625f5a0e705d98692e4e1db0ac3a5680d9fc
sampat646 <113638514+sampat646@users.noreply.github.com>
2024-05-28 19:40:36 +0530

    Create Add digit.cpp

---
+++ Digit problems/Add digit.cpp
@@ -0,0 +1,32 @@
+// Program 6: LeetCode Problem - Add Digits
+#include <iostream>
+
+using namespace std;
+
+/*
+This function takes an integer 'n' as input and applies the "Add Digits" logic repeatedly
+until the sum of digits becomes a single-digit number.
+It returns the final single-digit sum.
+*/
+int addDigits(int n) {
+    int sum;
+    do {
+        sum = 0;
+        while (n > 0) {
+            int lastDigit = n % 10;
+            sum += lastDigit;
+            n /= 10;
+        }
+        if (sum >= 10)
+            n = sum;
+    } while (sum >= 10);
+
+    return sum;
+}
+
+int main() {
+    int num = 199;
+    int result = addDigits(num);
+    cout << "The sum of digits for " << num << " is: " << result << endl;
+    return 0;
+}
