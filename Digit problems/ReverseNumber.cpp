172edac0b3a9094063a6039ce4dd944d273045fd
sampat646 <113638514+sampat646@users.noreply.github.com>
2024-05-28 19:34:04 +0530

    Create ReverseNumber.cpp

---
+++ Digit problems/ReverseNumber.cpp
@@ -0,0 +1,25 @@
+// Program 2: Reverse a Number
+#include <iostream>
+
+using namespace std;
+
+/*
+This function takes an integer 'n' as input and reverses the digits of the number.
+It returns the reversed number.
+*/
+int reverseNumber(int n) {
+    int reverse = 0;
+    while (n > 0) {
+        int lastDigit = n % 10;
+        n = n / 10;
+        reverse = (reverse * 10) + lastDigit;
+    }
+    return reverse;
+}
+
+int main() {
+    int num = 1227;
+    int reversedNum = reverseNumber(num);
+    cout << "Reverse of " << num << " is: " << reversedNum << endl;
+    return 0;
+}
